23f285fd304b7acaea971477192ea3b15c02a931
r-k-jonynas <e0191908+git@u.nus.edu>
2025-11-01 23:58:27 +0800

    implement basic syntax highlighting

---
+++ queries/highlights.scm
@@ -0,0 +1,94 @@
+; Highlight queries for ClickHouse grammar
+
+; Keywords
+[
+  "CREATE"
+  "TABLE"
+  "IF"
+  "NOT"
+  "EXISTS"
+  "ENGINE"
+  "DEFAULT"
+  "MATERIALIZED"
+  "ALIAS"
+  "CODEC"
+  "TTL"
+  "COMMENT"
+  "PARTITION"
+  "BY"
+  "ORDER"
+  "PRIMARY"
+  "KEY"
+  "SAMPLE"
+  "SETTINGS"
+  "AND"
+  "OR"
+  "LIKE"
+  "IN"
+  "IS"
+  "CAST"
+] @keyword
+
+; Data types - primitive
+(primitive_type) @type.builtin
+
+; Data types - complex type keywords
+[
+  "Array"
+  "Tuple"
+  "Map"
+  "Nested"
+  "Nullable"
+] @type.builtin
+
+; Engine names
+(engine_name) @type.builtin
+
+; String literals
+(string_literal) @string
+
+; Numbers
+(number) @number
+
+; Comments
+(comment) @comment
+
+; Identifiers
+(identifier) @variable
+
+; Function calls - first child is the identifier (function name)
+(function_call
+  . (identifier) @function)
+
+; Unary operators (operator is the first child)
+(unary_expression
+  . (_) @operator)
+
+; Punctuation - match only what exists in the grammar
+"(" @punctuation.bracket
+")" @punctuation.bracket
+"[" @punctuation.bracket
+"]" @punctuation.bracket
+"," @punctuation.delimiter
+"." @punctuation.delimiter
+"=" @punctuation.delimiter
+
+; Table and column names in CREATE TABLE
+(qualified_table_name
+  (identifier) @variable)
+
+; Column definition: first child is the identifier (column name)
+(column_definition
+  . (identifier) @variable)
+
+; Type in column definition
+(column_definition
+  (primitive_type) @type)
+(column_definition
+  (complex_type) @type)
+
+; Type names in complex types
+(complex_type
+  (primitive_type) @type)
+(complex_type
+  (complex_type) @type)
